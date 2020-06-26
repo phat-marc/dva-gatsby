e772b90afd9105d60f96fe70cc14a742fafd1cb7
It's meee! Phat Marc <phfatm@gmail.com>
2020-06-26 14:21:43 +0200

    update

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,8 +1,19 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+// Styled components
+const BlogLink = styled(Link)`
+text-decoration: none;
+`
+const BlogTitle = styled.h3`
+margin-bottom: 1rem;
+color: blue;
+`
+
 
 export default ({ data }) => {
 	console.log(data)
@@ -14,14 +25,15 @@ export default ({ data }) => {
 		    <h2>Da Vinci Cooking</h2>
 		    <p>This is where you will learn to cook Mediterranean style food at home</p>
 		    <h4>{data.allMarkdownRemark.totalCount}</h4>
-		    {
-		    	data.allMarkdownRemark.edges.map(({node}) => (
+		    {data.allMarkdownRemark.edges.map(({ node }) => (
 		    		<div	key={node.id}>
-		    			<h3>{node.frontmatter.title}</h3> 
-		    			<h4>{node.frontmatter.date}</h4>
+		    			<BlogLink to={node.fields.slug}>
+			    			<BlogTitle><h3>{node.frontmatter.title}</h3></BlogTitle> 
+			    			<p>{node.frontmatter.date}</p>
+		    			</BlogLink>
 		    			<p>{node.excerpt}</p>
 		    		</div>
-		    		))
+		    	))
 		    }
 	    </div>
 	    <Link to="/">Go back to the homepage</Link>
@@ -30,7 +42,7 @@ export default ({ data }) => {
 
 export const query = graphql`
 query {
-  allMarkdownRemark {
+  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC 	}) {
     edges {
       node {
         id
